83489ab81bb131eb9fefce395469d48f624dca61
RAKIBSaida <rakibsaida@gmail.com>
2020-02-16 12:36:03 +0000

    Create Converter.cs
    Co-Authored-By: rakibsaida <rakibsaida@users.noreply.github.com>

---
+++ prjt_wpf/prjt_wpf/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace prjt_wpf
+{
+    class Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                Binary binary = (Binary)value;
+                byte[] img = binary.ToArray();
+                return img;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
